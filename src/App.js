3b285cdd35f5894a38a68aa920cb41c3ea563102
dominthebox <dsallustro@gmail.com>
2021-07-07 16:54:44 -0400

    add form is working for my todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
-    super();
+    super();  // why is super crossed out here? marked as depreciated?
     this.state = {
-      input: ''
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
     }
   }
   
-  handleChanges = e => {
+  addItem = (taskName) => {
+    const newTodo = {
+      task: taskName,
+      id: new Date(),
+      completed: false
+    };
     this.setState({
-      input: e.target.value
+      todos: [...this.state.todos, newTodo]
     })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.addItem(this.state.input);
   }
 
   render() {
@@ -27,14 +40,8 @@ class App extends React.Component {
       <div>
         <h1>To-Do List</h1>
         <h2>What needs to get done today?</h2>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChanges}
-            type='text'
-            name='task' 
-            placeholder='What else?'
-          />
-          <button>Add it!</button>
-        </form>
+        <TodoList todos={this.state.todos} />
+        <TodoForm addItem={this.addItem} />
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ToDo = (props) => {
+    return(
+        <div>
+            <p>
+                {props.todo.task}
+            </p>
+        </div>
+    )
+}
+
+export default ToDo
